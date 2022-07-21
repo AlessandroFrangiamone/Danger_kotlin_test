c0850cfdf549624297332e52fe51e8eb7df08324
alessandrofrangiamone <a.frangiamone@reply.it>
2022-07-22 00:23:24 +0200

    Add Danger File

---
+++ Dangerfile.df.kts
@@ -0,0 +1,22 @@
+//@file:Repository("https://repo.maven.apache.org/maven2/")
+//@file:DependsOn("org.apache.commons:commons-text:1.6")
+
+import systems.danger.kotlin.*
+
+danger(args) {
+    // Empty dangerFile
+    val allSourceFiles = git.modifiedFiles + git.createdFiles
+    //val changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+    //val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
+
+    onGitHub {
+        //val isTrivial = pullRequest.title.contains("#trivial")
+
+        // Big PR Check
+        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 800) {
+            warn("Big PR, try to keep changes smaller if you can")
+        }
+        
+    }
+    
+}
